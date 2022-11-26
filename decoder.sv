c87f81bf2c5750c4dc5f151d0d5f3e3d219c7036
JoyenBenitto <joyen.benitto12@gmail.com>
2022-11-26 11:26:34 +0530

    adding Decoder

--- decoder.sv
+++ decoder.sv
@@ -15,14 +15,14 @@ begin
 						out[14:10] =   instruction[11:7];		 //rd	
 						out[17:15] =   instruction[14:12];      //funct3
 						out[23:16] =   instruction[31:25];      //funct7
-						out[31:24] = 7'b0000000;
+						out[31:24] = 7'bz;
 			 			end
 		7'b0010011: begin
 						out[4:0]   =   instruction[19:15];      //I-type instruction -- rs1
 						out[17:5]  =   instruction[31:20];      //immediate
 						out[23:18] =   instruction[11:7];		 //rd	
 						out[26:24] =   instruction[14:12];      //funct3
-						out[31:27] = 7'b0000000;
+						out[31:27] = 7'bz;
 						end
 						
       default:begin
